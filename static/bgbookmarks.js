ee60cf5011fc51264149cf71ca56316a1453ce59
Scikous <95752605+Scikous@users.noreply.github.com>
2024-02-11 17:42:41 +0200

    Add, delete, choose default folder, keyboard shortcuts, see folders for bookmark, all created

---
+++ static/bgbookmarks.js
@@ -0,0 +1,77 @@
+// Function to save the bookmark to the default folder without opening popup
+function saveBookmarkBG() {
+    chrome.storage.sync.get(['defaultFolderId'], function(result) {
+        const defaultFolderId = result.defaultFolderId;
+
+        if (defaultFolderId) {
+            chrome.tabs.query({ 'currentWindow': true, 'highlighted': true }, function(tabs) {
+                // Check if there are any highlighted tabs
+                if (tabs && tabs.length > 0) {
+                    // Iterate through the highlighted tabs
+                    tabs.forEach(function(tab) {
+                        // Create a new bookmark object
+                        var bookmark = {
+                            parentId: defaultFolderId,
+                            title: tab.title,
+                            url: tab.url
+                        };
+        
+                        // Save the bookmark using Chrome's bookmarks API
+                        chrome.bookmarks.create(bookmark, function(result) {
+                            // log result
+                           console.log('Bookmark saved:', result);
+                        });
+                    });
+                }
+            });
+        }
+    });
+}
+
+// Function to save the bookmark to the selected folder
+function deleteBookmarkBG() {
+
+    chrome.storage.sync.get(['defaultFolderId'], function(result) {
+        const defaultFolderId = result.defaultFolderId;
+
+        if (defaultFolderId) {
+            chrome.tabs.query({ 'currentWindow': true, 'highlighted': true }, function(tabs) {
+                // Check if there are any highlighted tabs
+                if (tabs && tabs.length > 0) {
+                    // Iterate through the highlighted tabs
+                    tabs.forEach(function(tab) {
+                        // Create a new bookmark object
+                        var bookmark = {
+                            parentId: defaultFolderId,
+                            title: tab.title,
+                            url: tab.url
+                        };
+
+                        // Delete the bookmark
+                        chrome.bookmarks.search({ title: bookmark.title, url: bookmark.url }, function(results) {
+                            if (results.length > 0) {
+                                chrome.bookmarks.remove(results[0].id, function() {
+                                console.log('Bookmark saved:', results[0], results.length);
+                                    });
+                                }
+                            });
+                    });
+                }
+            });
+        }
+    });
+}
+
+chrome.commands.onCommand.addListener(function(command) {
+    if (command === "save-bookmark") {
+        saveBookmarkBG();
+    }
+    else if (command === "delete-bookmark"){
+        deleteBookmarkBG();
+    }
+});
+
+// Listen for messages from content scripts or other parts of the extension
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(message); // Log the message received
+});
